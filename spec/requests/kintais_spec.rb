2c4ddccfb6967098537695ac6a86f893ab6e112c
MinhUchiha <nqminh27894@gamil.com>
2018-09-12 16:11:38 +0700

    edit rspec request for main page

--- spec/requests/kintais_spec.rb
+++ spec/requests/kintais_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Kintais', type: :request do
   describe 'GET /kintais' do
-    it 'works! (now write some real specs)' do
+    before do
+      user = FactoryBot.create :user, password: 'abc123', admin: true
+      post login_path, params: { session: { 担当者コード: user.id, password: 'abc123' } }
+    end
+    it 'index' do
       get kintais_path
-      expect(response).to have_http_status(302)
+      expect(response).to render_template(:index)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'sumikakunin' do
+      get sumikakunin_kintais_path
+      expect(response).to render_template(:sumikakunin)
+      expect(response).to have_http_status(200)
     end
   end
 end
