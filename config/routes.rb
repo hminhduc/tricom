643c0b38a53f3e1fac9f5e84c1ba58b7435abc9f
hminhduc <hminhduc@gmail.com>
2019-03-05 17:29:51 +0700

    March 5, 2019

--- config/routes.rb
+++ config/routes.rb
@@ -75,7 +75,7 @@ Jpt::Application.routes.draw do
   end
 
   resources :kintais do
-    collection { get :search, :pdf_show, :sumikakunin }
+    collection { get :search, :pdf_show, :sumikakunin, :summary }
     collection { post :import, :ajax }
     collection { get :export_csv, :export_pdf }
   end
