2c0eaedc7b66609ff6bb5f4094d9631ab874016d
Nguyen Dinh Hung <hung.nguyen.dinh@bunbunsoft.com>
2019-03-11 20:44:26 +0700

    add View:Kintaishuukei (them button Excel va CSV)

--- app/assets/javascripts/kintais/summary.js
+++ app/assets/javascripts/kintais/summary.js
@@ -18,10 +18,23 @@ $(function () {
     });
 // add for datatable
     $('#kintaisummary').DataTable({
+        "dom": "<'row'<'col-md-6'l><'col-md-6'f>><'row'<'col-md-7'B><'col-md-5'p>><'row'<'col-md-12'tr>><'row'<'col-md-12'i>>",
         "pagingType": "full_numbers",
         "oLanguage": {
             "sUrl": $path
         },
+        "buttons": [
+            {
+                "extend":    'excelHtml5',
+                "text":      '<i class="fa fa-file-excel-o"></i>',
+                "titleAttr": 'Excel'
+            },
+            {
+                "extend":    'csvHtml5',
+                "text":      '<i class="fa fa-file-text-o"></i>',
+                "titleAttr": 'CSV'
+            }
+        ],
         pageLength: 100
     });
 });
