eed0bc48b37540fd3129aa4ac2ff380e2b2d0abe
NguyenDinhHung <nguyendinhhung.top@gmail.com>
2020-07-16 08:12:13 +0700

    fix some bug

--- app/models/kairanshosai.rb
+++ app/models/kairanshosai.rb
@@ -3,7 +3,7 @@ class Kairanshosai < ApplicationRecord
   CSV_HEADERS = %w(回覧コード 対象者 created_at updated_at 状態)
   # include PgSearch
   # multisearchable against: %w{回覧コード 対象者 created_at updated_at 状態}
-  belongs_to :kairan, foreign_key: :回覧コード, class_name: Kairan
+  belongs_to :kairan, foreign_key: :回覧コード, class_name: 'Kairan'
   belongs_to :shainmaster, foreign_key: :対象者
 
   delegate :要件, to: :kairan, allow_nil: true
